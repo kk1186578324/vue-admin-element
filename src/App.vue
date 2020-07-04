803dd4e6503fb0284b28280c39f16347af956e2e
‘ <lk1186578324@163.com>
2020-07-04 20:44:29 +0800

    添加element-ui

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    {{obj.mag}}
+    {{obj.yc}}
+    {{mag}}
+    <button @click="btnClick">dsasda</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+    data(){
+      return{
+          obj:{
+              mag:'12fsdf'
+          },
+          mag:'qwrewrewerwe'
+
+      }
+
+    },
+  components: {
+    HelloWorld
+  },
+
+  methods:{
+
+      btnClick(){
+      }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
